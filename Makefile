dc11880920aac5e9b37c4b085b14c029f8498b0a
kpierzynski@hotmail.com <kpierzynski@hotmail.com>
2019-05-24 17:09:57 +0200

    Version 1.0 of clock project

---
+++ Makefile
@@ -0,0 +1,4 @@
+PROGRAM=clock
+EXTRA_COMPONENTS = extras/ws2812 extras/sntp extras/i2s_dma extras/ws2812_i2s
+
+include ../../common.mk
---
+++ clock.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <esp/uart.h>
+#include <espressif/esp_common.h>
+
+#include <ssid_config.h>
+
+#include <sntp.h>
+#include <ws2812_i2s/ws2812_i2s.h>
+
+/* Delay with RTos define */
+#define delay_ms(ms) vTaskDelay((ms) / portTICK_PERIOD_MS)
+/* */
+
+/* */
+#define LED_COUNT 60
+#define NTP_REFRESH 16
+#define TIMER_FREQ_HZ 1
+/* */
+
+/* Variables */
+time_t ts;
+struct tm *tms;
+
+ws2812_pixel_t pixels[LED_COUNT];
+/* */
+
+/* Functions */
+ws2812_pixel_t color_set( uint8_t r, uint8_t g, uint8_t b ) {
+    ws2812_pixel_t color = {{b,g,r,0}};
+    return color;
+}
+
+ws2812_pixel_t color_add( ws2812_pixel_t a, ws2812_pixel_t b) {
+    ws2812_pixel_t color = {{a.blue + b.blue, a.green + b.green, a.red + b.red, 0}};
+    return color;
+}
+/* */
+
+/* Timer for updating clock */
+void clock_update( void * arg ) {
+    /* Time refresh */
+    ts = time(NULL);
+    tms = localtime(&ts);
+    /* */
+
+    /* Debug time printf */
+    printf("TIME: %d %d %d\n", tms->tm_hour, tms->tm_min, tms->tm_sec );
+    /* */
+    
+    /* Clear all leds */
+    memset(pixels, 0, sizeof(ws2812_pixel_t) * LED_COUNT );
+    /* */
+
+    /* Turn on diodes based on time */
+    pixels[(((tms->tm_hour*5) % LED_COUNT)+(tms->tm_min/15)) % LED_COUNT] = color_set(32,0,0);
+    pixels[tms->tm_min % LED_COUNT] = color_add(color_set(0,8,0), pixels[tms->tm_min]);
+    pixels[tms->tm_sec % LED_COUNT] = color_add(color_set(0,0,16),pixels[tms->tm_sec]);
+    /* */
+
+    /* Push data to leds */
+    ws2812_i2s_update(pixels, PIXEL_RGB);
+    /* */
+}
+/* */
+
+/* Configuration task */
+void configuration( void * pvParameters ) {
+
+    /* Wait for wifi connection*/
+    while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) {
+	    delay_ms(50);
+	}
+    /* */
+    
+    /* Some ntp servers*/
+    const char *servers[] = {"0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org"};
+    /* */
+
+    /* Setup ntp*/
+    LOCK_TCPIP_CORE();                                              //Lock core for sync tcpip thread
+        sntp_set_update_delay(NTP_REFRESH*60000);                   //NTP server update each NTP_REFRESH minute
+        const struct timezone tz = {2*60, 0};                       //Set timezone 
+        sntp_initialize(&tz);                                       //Init ntp
+        sntp_set_servers(servers, sizeof(servers) / sizeof(char*)); //Pass servers to ntp module
+    UNLOCK_TCPIP_CORE();                                            //Unlock core
+    /* */
+
+    /* Init ws2818 bus on i2s */
+    ws2812_i2s_init(LED_COUNT, PIXEL_RGB);
+    /* */
+
+    /* Setup timer with 1Hz frequency */
+    timer_set_interrupts(FRC1, false);
+    timer_set_run(FRC1, false);
+
+    _xt_isr_attach(INUM_TIMER_FRC1, clock_update, NULL);
+
+    timer_set_frequency(FRC1, TIMER_FREQ_HZ);
+    timer_set_interrupts(FRC1, true);
+
+    timer_set_run(FRC1, true);
+    /* */
+
+    /* Delete configuration task */
+    printf("Config task done.\n");
+    vTaskDelete(NULL);
+    /* */
+}
+/* */
+
+/* void main( void ) */
+void user_init(void)
+{
+    /* Configure uart */
+    uart_set_baud(0, 115200);
+    /* */
+
+    /* Configure ssid and password for wifi sta connection */
+    struct sdk_station_config config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASS,
+    };
+    /* */
+
+    /* Setup wifi */
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&config);
+    /* */
+
+    /* Create task for configuration */
+    xTaskCreate(&configuration, "configuration", 1024, NULL, 1 | portPRIVILEGE_BIT, NULL);
+    /* */
+}
+/* */
